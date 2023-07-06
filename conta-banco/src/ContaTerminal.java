876ee1e1a047a7d1981a491cb1fdab1f85ce08e2
AlexsandroAmancio85 <alexsandroamancio85@gmail.com>
2023-07-05 22:18:44 -0300

    implementação da regra de négocio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         // TODO: Conhecer e importar a classe Scanner
@@ -8,6 +10,20 @@ public class ContaTerminal {
 
         // Exibir a mensagem de conta criada
 
+        Scanner nomecliente = new Scanner(System.in);
+        System.out.println("Por favor digite seu nome: ");
+
+        Scanner agencia = new Scanner(System.in);
+        System.out.println("Por favor digite o número da sua agência: ");
+
+        Scanner conta = new Scanner(System.in);
+        System.out.println("Por favor digite o número da sua conta: ");
+
+        Scanner saldo = new Scanner(System.in);
+        System.out.println("Informe qual valor gostaria de depositar: ");
+
+        System.out.println("Olá " + nomecliente + "," + "obrigado por criar sua conta em nosso banco, sua agência é " + agencia + "," + "conta " + conta + "e seu saldo disponivel é: " + saldo);
+        
 
         
 
